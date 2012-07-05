5f68ced3b6881310ec3b67bc4277ac6c4148820a
Thomas Traub <thomastraub2000@yahoo.de>
2012-07-05 16:08:31 +0200

    Add customized application files
    - separation of HTML view and content

---
+++ controllers/welcome.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$data = array
+		(
+			'content' => $this->load->view('pages/index', false, true)
+		);
+		
+		$this->load->view('main_view', $data);
+	}
+}
---
+++ views/main_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" type="text/css" media="all" href="assets/doc_style/all.css" />
+</head>
+<body>
+
+<?php
+
+	echo $content;
+
+?>
+</body>
+</html>

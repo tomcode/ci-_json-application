97162d2adf21d627b67bfccb2c1f683fa5da985d
Thomas Traub <thomastraub2000@yahoo.de>
2012-07-09 18:39:31 +0200

    Add HTML/JSON switch to json_common_error.php
    - requires PHP 5 > 5.2.0 (filter_var)
    - based on HTTP_X_REQUESTED_WITH === XMLHttpRequest

--- errors/json_common_error.php
+++ errors/json_common_error.php
@@ -1,5 +1,7 @@
 <?php
 
+	// Normailze the different variables passed
+	
 	$error_obj->title = isset($title) ? $title : '';
 	$error_obj->heading = isset($heading) ? $heading : '';
 	$error_obj->message = isset($message) ? strip_tags($message) : '';
@@ -9,6 +11,78 @@
 	if(isset($line) && $line) $error_obj->line =  $line;
 
 
-	header("Content-Type: application/json; charset=UTF-8", true);
+	$is_ajax = false;
+	
+	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']))
+	{
+		$http_x_requested_with = filter_var($_SERVER['HTTP_X_REQUESTED_WITH']);
+		
+		$is_ajax = $http_x_requested_with === 'XMLHttpRequest';
+	}
 
-	echo json_encode($error_obj);
+	if($is_ajax)
+	{
+		header("Content-Type: application/json; charset=UTF-8", true);
+
+		echo json_encode($error_obj);
+		
+		exit;
+	}
+	
+	
+	if(isset($error_obj->severity)) :
+	
+	// This is a PHP error
+	
+?><div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+
+<h4>A PHP Error was encountered</h4>
+
+<p>Severity: <?php echo $severity; ?></p>
+<p>Message:  <?php echo $message; ?></p>
+<p>Filename: <?php echo $filepath; ?></p>
+<p>Line Number: <?php echo $line; ?></p>
+
+</div><?php
+
+	else :
+	
+	// The other errors 
+
+?><html>
+<head>
+<title><?php echo $error_obj->title?></title>
+<style type="text/css">
+
+body {
+background-color:	#fff;
+margin:				40px;
+font-family:		Lucida Grande, Verdana, Sans-serif;
+font-size:			12px;
+color:				#000;
+}
+
+#content  {
+border:				#999 1px solid;
+background-color:	#fff;
+padding:			20px 20px 12px 20px;
+}
+
+h1 {
+font-weight:		normal;
+font-size:			14px;
+color:				#990000;
+margin: 			0 0 4px 0;
+}
+</style>
+</head>
+<body>
+	<div id="content">
+		<h1><?php echo $error_obj->heading; ?></h1>
+		<?php echo $error_obj->message; ?>
+	</div>
+</body>
+</html>
+<?php
+
+	endif;
